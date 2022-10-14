541dbd62fbbb9c6ba46a3bf0c69b86daa3ee5b2f
Riddhi bhatt <riddhi2211bhatt@gmail.com>
2022-10-14 11:48:59 +0530

    just added a file

---
+++ contact-app/src/components/ContactList.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import { useRef } from 'react'
+import ContactCard from './ContactCard'
+const ContactList = (props) => {
+  const inputEl = useRef("")
+      const deleteContactHandler = (id) => {
+      props.getContactId(id)
+    } 
+    const renderContactList = props.contacts.map((contact)=>{
+        return(
+            <ContactCard 
+              contact={contact} 
+              clickHandler={deleteContactHandler} 
+              key={contact.id}
+            />
+        )
+    })
+    const getSearchTerm = (e) => {
+      props.searchKeyword(inputEl.current.value)    
+    }
+
+  return (
+    <div>
+      <div class="main">
+          <h2>Contact List
+            <Link to="/add">
+              <button className="ui button blue right">Add Contact</button>
+            </Link>
+          </h2>
+      <div className="ui search">
+          <div className="ui icon input">
+            <input ref={inputEl} type="text" placeholder="Search Contacts" className="prompt" value={props.term} onChange={getSearchTerm}></input>
+          <i className='search icon'></i>
+          </div>
+      </div>
+      </div>
+      <div className="ui celled list">{renderContactList.length > 0 ? renderContactList: "No Contacts Available"}</div>
+    </div>
+    
+  )
+}
+
+export default ContactList  
